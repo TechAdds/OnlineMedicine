529ec0968dc665dc6f44d45343d4f6fa60437c49
Sanjana Sanikommu <sanjanasanikommu@gmail.com>
2022-10-22 18:07:15 +0530

    db

---
+++ database/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA1vMqwc_XdR8ajPEf1PW6a3iue9LEoh7E",
+  authDomain: "fir-auth-37b55.firebaseapp.com",
+  projectId: "fir-auth-37b55",
+  storageBucket: "fir-auth-37b55.appspot.com",
+  messagingSenderId: "776518037492",
+  appId: "1:776518037492:web:71ed04293cc7b32f10d3a1",
+  measurementId: "G-ZB6ZVHBGNZ",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
